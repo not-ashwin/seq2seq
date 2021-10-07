06eb1a1e48a7a464afe9ddbeb13f56f53c23a690
not-ashwin <bagreeashwin@gmail.com>
2021-10-07 20:25:12 +0800

    Add file for trainer

--- trainer.py
+++ trainer.py
@@ -1,24 +1,3 @@
 from seq2seq import Encoder, Decoder, Seq2Seq
 
-TRAIN_DATA = [
-              "Walmart is a leading e-commerce company",
-              "I reached Chennai yesterday.",
-              "I recently ordered a book from Amazon",
-              "I was driving a BMW",
-              "I ordered this from ShopClues",
-              "Fridge can be ordered in Amazon ",
-              "I bought a new Washer",
-              "I bought a old table",
-              "I bought a fancy dress",
-              "I rented a camera",
-              "I rented a tent for our trip",
-              "I rented a screwdriver from our neighbour",
-              "I repaired my computer",
-              "I got my clock fixed",
-              "I got my truck fixed",
-              "Flipkart started it's journey from zero",
-              "I recently ordered from Max",
-              "Flipkart is recognized as leader in market",
-              "I recently ordered from Swiggy",
-              ]
-
+#TODO: develop trainer for training seq2seq
